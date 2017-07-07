33ddd243f35a143470608d8eb7b82c8bb0adc417
mengguiyouziyi <775618369@qq.com>
2017-07-07 15:09:40 +0800

    all-2

--- lg_scrp_170704/lagou/lagou/middlewares.py
+++ lg_scrp_170704/lagou/lagou/middlewares.py
@@ -29,8 +29,8 @@ proxyPass = "3018672C5A167A3D"
 proxyUser = "HWAP93ES770B921D"
 proxyPass = "FD67384CCCADBF04"
 """
-proxyUser = "H8J738EHR4H5GE5D"
-proxyPass = "3018672C5A167A3D"
+proxyUser = "H24CFQ64JP06V1WD"
+proxyPass = "FA1D98DF8F3E55FF"
 
 # for Python3
 proxyAuth = "Basic " + base64.urlsafe_b64encode(bytes((proxyUser + ":" + proxyPass), "ascii")).decode("utf8")
