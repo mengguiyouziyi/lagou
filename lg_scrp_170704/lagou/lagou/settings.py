8e310d79b121398225cd245be1a4ccbd18fbe3d2
mengguiyouziyi <775618369@qq.com>
2017-07-07 13:11:57 +0800

    all-2

--- lg_scrp_170704/lagou/lagou/settings.py
+++ lg_scrp_170704/lagou/lagou/settings.py
@@ -12,10 +12,12 @@ import os
 import sys
 from os.path import dirname
 
-base_path = dirname(dirname(os.path.abspath(os.path.dirname(__file__))))
-path = dirname(os.path.abspath(os.path.dirname(__file__)))
+father_path = dirname(dirname(os.path.abspath(dirname(__file__))))
+base_path = dirname(dirname(os.path.abspath(dirname(__file__))))
+path = dirname(os.path.abspath(dirname(__file__)))
 sys.path.append(path)
 sys.path.append(base_path)
+sys.path.append(father_path)
 
 BOT_NAME = 'lagou'
 
