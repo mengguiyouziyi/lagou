ab288789736ab574b645711e795a179acc68ba47
mengguiyouziyi <775618369@qq.com>
2017-07-08 18:43:46 +0800

    all-3

--- lg_scrp_170708/lagou/lagou/middlewares.py
+++ lg_scrp_170708/lagou/lagou/middlewares.py
@@ -30,11 +30,11 @@ proxyServer = "http://proxy.abuyun.com:9020"
 # proxyUser = "HWAP93ES770B921D"
 # proxyPass = "FD67384CCCADBF04"
 
-proxyUser = "H24CFQ64JP06V1WD"
-proxyPass = "FA1D98DF8F3E55FF"
+# proxyUser = "H24CFQ64JP06V1WD"
+# proxyPass = "FA1D98DF8F3E55FF"
 #
-# proxyUser = "HQ78N3Y82239165D"
-# proxyPass = "AA99073C3271DBFA"
+proxyUser = "HQ78N3Y82239165D"
+proxyPass = "AA99073C3271DBFA"
 #
 # proxyUser = "H8963415MP59046D"
 # proxyPass = "6C586451622880CB"
